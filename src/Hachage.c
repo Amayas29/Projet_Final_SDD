5d544cc17a19814caf7094c557e97bf16be03a2e
HamidKolli <gamog5756@gmail.com>
2021-03-25 12:30:09 +0100

    add creer and liberer hash table

--- src/Hachage.c
+++ src/Hachage.c
@@ -14,3 +14,32 @@ int hachage(int key, int lenght) {
     float a = (sqrt(5) - 1) / 2.0;
     return ((int)(lenght * (key * a - (int)(key * a))));
 }
+
+TableHachage *cree_table_hachage(int taille){
+    TableHachage *table = (TableHachage *) malloc(sizeof(TableHachage));
+    if(!table) {
+        print_probleme("Erreur d'allocation");
+        return NULL;
+    }
+    table->lenght = taille;
+    table->table = (CellNoeud *) malloc(sizeof(CellNoeud) * taille);
+    
+    if(!table->table){
+        free(table);
+        print_probleme("Erreur d'allocation");
+        return NULL;
+    }
+    for(int i = 0;i <taille ;i++)
+        table->table[i] = NULL;
+
+    return table;
+}
+
+void liberer_table_hachage(TableHachage *table){
+    if(!table->table) return;
+
+    for(int i = 0;i <table->lenght ;i++)
+        liberer_cell_noeuds(table->table[i],0);
+
+    free(table);
+}
