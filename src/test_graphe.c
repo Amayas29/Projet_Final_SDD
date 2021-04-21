e31c82efc9d6077226af6dc7503016496ec58e96
Amayas29 <amayas29sd@gmail.com>
2021-04-21 15:54:20 +0200

    Add test reorganisation

--- src/test_graphe.c
+++ src/test_graphe.c
@@ -3,23 +3,34 @@
 #include "Graphe.h"
 #include "commun.h"
 
-int main() {
-    FILE *file = fopen("../resources/00014_burma.cha", "r");
-    if (!file) {
-        print_probleme("Fichier introuvable");
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr,
+                "Usage: %s <filename>\n", argv[0]);
         return 1;
     }
 
-    Chaines *chaines = lecture_chaines(file);
-    if (!chaines) {
-        fclose(file);
+    argc--;
+    argv++;
+
+    FILE *file_reseau = fopen(argv[0], "r");
+
+    if (!file_reseau) {
+        print_probleme("Erreur d'ouverture du fichier");
         return 1;
     }
+
+    Chaines *chaines = lecture_chaines(file_reseau);
+    if (!chaines) {
+        print_probleme("Erreur de création");
+        fclose(file_reseau);
+        return 1;
+    }
+    fclose(file_reseau);
 
     Reseau *reseau = reconstitue_reseau_liste(chaines);
     if (!reseau) {
         liberer_structure(chaines);
-        fclose(file);
         return 1;
     }
 
@@ -29,22 +40,8 @@ int main() {
         print_probleme("Erreur de création");
         liberer_structure(chaines);
         liberer_reseau(reseau);
-        fclose(file);
         return 1;
     }
-
-    ListeEntier liste;
-
-    generate_plus_petit_chaine(graphe, 5, 11, &liste);
-
-    for (Cell_entier *curr = liste; curr; curr = curr->suiv) {
-        printf("%d", curr->u);
-        if (curr->suiv)
-            printf(" <-> ");
-    }
-    printf("\n");
-
-    desalloue(&liste);
 
     int res = reorganise_reseau(reseau);
     printf("%d \n", res);
@@ -52,7 +49,6 @@ int main() {
     liberer_structure(chaines);
     liberer_reseau(reseau);
     liberer_graphe(graphe);
-    fclose(file);
 
     return 0;
 }
