1977b80e15880d23eb3ee7b347c6954bf356a890
HamidKolli <gamog5756@gmail.com>
2021-04-26 15:08:56 +0200

    modify ChaineMain and add SVG capture

--- src/ChaineMain.c
+++ src/ChaineMain.c
@@ -4,11 +4,23 @@
 #include "commun.h"
 
 int main(int argc, char const *argv[]) {
-    // On ouvre le fichier
-    FILE *fichier = fopen("../resources/00014_burma.cha", "r");
+    // On test les argument
+    if (argc != 2) {
+        fprintf(stderr,
+                "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
 
-    if (!fichier) {
-        print_probleme("Fichier introuvable");
+    // On supprime le nom du fichier en argument
+    argc--;
+    argv++;
+
+    // On ouvre le fichier de chaines
+    FILE *fichier = fopen(argv[0], "r");
+
+    // On teste l'ouverture du fichier
+    if(!fichier){
+        print_probleme("Erreur d'ouverture du fichier");
         return 1;
     }
 
@@ -23,10 +35,10 @@ int main(int argc, char const *argv[]) {
     }
 
     // On affiche les chaines
-    affiche_chaines_SVG(chaines, "chaines");
+    affiche_chaines_SVG(chaines, "chaines_avant_ecriture");
 
     // On ouvre un fichier en ecriture
-    fichier = fopen("ecriture_burma.cha", "w");
+    fichier = fopen("resultat_ecriture.cha", "w");
     if (!fichier) {
         print_probleme("Erreur d'ouverture du fichier");
         liberer_structure(chaines);
@@ -38,6 +50,30 @@ int main(int argc, char const *argv[]) {
     fclose(fichier);
 
     liberer_structure(chaines);
+
+
+    // On ouvre le fichier de chaines
+    fichier = fopen("resultat_ecriture.cha", "r");
+    if (!fichier) {
+        print_probleme("Erreur d'ouverture du fichier");
+        return 1;
+    }
+
+    // On lit depuis le fichier
+    chaines = lecture_chaines(fichier);
+
+    fclose(fichier);
+
+    if (!chaines) {
+        print_probleme("Erreur de creation");
+        return 1;
+    }
+
+    // On affiche les chaines
+    affiche_chaines_SVG(chaines, "chaines_apres_ecriture");
+
+    liberer_structure(chaines);
+
 
     return 0;
 }
